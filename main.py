0ff57c600488cfffc9a537f411700457fa975e8a
ales.novotny@seznam.cz <ales.novotny@seznam.cz>
2022-08-30 21:52:53 +0200

    new function

--- main.py
+++ main.py
@@ -9,8 +9,14 @@ def print_hi(name):
     print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
 
 
+def my_function(a,b):
+    return a + b
+
+
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
     print_hi('PyCharm')
+    print(my_function(4, 6))
+
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
